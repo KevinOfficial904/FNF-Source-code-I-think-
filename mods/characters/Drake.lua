6471089b3b7cc641556869296506c76f840e6b1c
LUCARIOOO123 <lucasianimatii123@gmail.com>
2022-08-18 18:50:54 +0300

    a

---
+++ mods/characters/Drake.lua
@@ -0,0 +1,36 @@
+strums = nil
+characterBool = false
+both = false
+function onCreatePost()
+
+    if getProperty('dad.curCharacter') == 'Drake' then
+        strums = 'opponent'
+        characterBool = false
+    elseif getProperty('boyfriend.curCharacter') == 'bf' then
+        strums = 'player'
+        characterBool = true
+    end
+    if getProperty('boyfriend.curCharacter') == 'bf' and getProperty('dad.curCharacter') == 'Drake' then
+        both = true
+    end
+
+    for i=0,3 do
+        if not both then
+            setPropertyFromGroup(strums..'Strums', i, 'texture', 'NOTE_assets')
+        else
+            setPropertyFromGroup('opponentStrums', i, 'texture', 'drakeNOTE_assets')
+            setPropertyFromGroup('playerStrums', i, 'texture', 'NOTE_assets')
+        end
+    end
+
+    for i=0,getProperty('unspawnNotes.length')-1 do
+        if not both then
+            if getPropertyFromGroup('unspawnNotes', i, 'mustPress') == characterBool then
+                setPropertyFromGroup('unspawnNotes', i, 'texture', 'NOTE_assets')
+            end
+        else
+            setPropertyFromGroup('unspawnNotes', i, 'texture', 'NOTE_assets')
+        end
+    end
+
+end
